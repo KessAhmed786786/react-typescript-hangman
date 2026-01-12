51f0a97174b80fbac091a6bdbef2fbbe72fe2a5c
KellerKezzer786 <kessahmed786@gmail.com>
2026-01-12 02:22:33 +0000

    Implemented logic for guessed word and letters

--- hangman/src/App.tsx
+++ hangman/src/App.tsx
@@ -1,5 +1,19 @@
+import { useState } from "react";
+import words from "./wordList.json";
+
 function App() {
-  return <h1>Hi</h1>;
+  // Create state word to guess
+  const [wordToGuess, setWordToGuess] = useState(() => {
+    // Inline function to return the array position randomnly
+    return words[Math.floor(Math.random() * words.length)];
+  });
+
+  // Create state to track letters guessed
+  const [guessedLetters, setGuessedLetters] = useState<string[]>([]);
+
+  console.log(wordToGuess);
+  return <h1>{wordToGuess}</h1>;
+  return <h1>{guessedLetters}</h1>;
 }
 
 export default App;
